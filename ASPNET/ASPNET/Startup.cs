95a94a598a4ffccc389cf713d45a0e9649ae6ecf
Lencsés Dávid <lencses.david77@gmail.com>
2020-11-17 14:14:18 +0100

    startup modositas

--- ASPNET/ASPNET/Startup.cs
+++ ASPNET/ASPNET/Startup.cs
@@ -16,7 +16,7 @@ namespace ASPNET
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,15 +27,10 @@ namespace ASPNET
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvcWithDefaultRoute();
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            
         }
     }
 }
