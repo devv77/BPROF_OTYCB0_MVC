72bcdc5f968d77496e1cb723d42aa748826ebc1d
Lencsés Dávid <lencses.david77@gmail.com>
2020-11-17 15:03:23 +0100

    Data kesz

---
+++ ASPNET/Data/Data.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+
+
+
+
+
+namespace Data
+{
+    public class DriversDbContext:DbContext
+    {
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DriversDbContext(DbContextOptions<DriversDbContext> opt):base(opt)
+        {
+
+        }
+
+        public DriversDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies().
+                    UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\Driver.mdf;integrated security=True;MultipleActiveResultSets=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Driver>(entity =>
+            {
+                entity
+                .HasOne(Driver => Driver.OwnTeam)
+                .WithMany(Team => Team.Drivers)
+                .HasForeignKey(Driver => Driver.TID);
+            });
+            modelBuilder.Entity<Team>(entity =>
+            {
+                entity
+                .HasOne(Team => Team.League)
+                .WithMany(League => League.Teams)
+                .HasForeignKey(Team => Team.LID);
+            });
+        }
+    }
+}
